29c715bbd12ee87f304428243f72ae8654284930
tankicagoneva <tankicaspasev@gmail.com>
2023-07-08 23:41:23 +0100

    feat: introduce movie info bar component

--- src/components/elements/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar.js
@@ -1,6 +1,51 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClock,
+  faMoneyBill,
+  faTicket,
+} from "@fortawesome/free-solid-svg-icons";
+import { calcTime, convertMoney } from "../../helpers";
 
-const MovieInfoBar = () => <div>MovieInfoBar</div>;
+import { StyledMovieInfoBar } from "../styles/StyledMovieInfoBar";
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+  <StyledMovieInfoBar>
+    <div className="movieinfobar-content">
+      <div className="movieinfobar-content-col">
+        <FontAwesomeIcon
+          icon={faClock}
+          style={{ color: "#ffffff" }}
+          size="2x"
+        />
+        <span className="movieinfobar-info">
+          Running time: {calcTime(time)}
+        </span>
+      </div>
+
+      <div className="movieinfobar-content-col">
+        <FontAwesomeIcon
+          icon={faMoneyBill}
+          size="2x"
+          style={{ color: "#ffffff" }}
+        />
+        <span className="movieinfobar-info">
+          Budget: {convertMoney(budget)}
+        </span>
+      </div>
+
+      <div className="movieinfobar-content-col">
+        <FontAwesomeIcon
+          icon={faTicket}
+          size="2x"
+          style={{ color: "#ffffff" }}
+        />
+        <span className="movieinfobar-info">
+          Revenue:{convertMoney(revenue)}
+        </span>
+      </div>
+    </div>
+  </StyledMovieInfoBar>
+);
 
 export default MovieInfoBar;
-
---
+++ src/components/styles/StyledMovieInfoBar.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const StyledMovieInfoBar = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  min-height: 100px;
+  height: auto;
+  background: #1c1c1c;
+  padding: 20px;
+  box-sizing: border-box;
+  font-family: "Abel", sans-serif;
+  font-size: 20px;
+  .movieinfobar-content {
+    max-width: 1280px;
+    width: 100%;
+    margin: 0 auto;
+    color: #fff;
+  }
+  .movieinfobar-content-col {
+    float: left;
+    width: 33.33%;
+    box-sizing: border-box;
+    padding: 10px 20px 0 0;
+  }
+  .movieinfobar-info {
+    padding: 5px 0 0 10px;
+    float: left;
+  }
+  .fa-clock,
+  .fa-ticket {
+    float: left;
+    margin-top: -4px;
+  }
+  .fa-money-bill {
+    float: left;
+    margin-top: -3px;
+  }
+  @media screen and (max-width: 768px) {
+    .fa-clock,
+    .fa-ticket,
+    .fa-money-bill {
+      display: none;
+    }
+  }
+  @media screen and (max-width: 425px) {
+    font-size: 14px;
+  }
+`;
